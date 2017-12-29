9280eef649297c902b9c24e467ec2bb575deaf34
WildCat <wildcat@catmbp.local>
2017-12-28 19:42:38 -0800

    Updates

--- src/utils/data.js
+++
@@ -1,8 +0,0 @@
-/**
- * Build the URI for data fetching
- * @param {string} currency 
- * @param {string} date 
- */
-export function buildURI(currency = 'EURGBP', date='20150101') {
-    return `https://raw.githubusercontent.com/FXclub/data/master/${currency}/${currency}_${date}.csv`;
-}
